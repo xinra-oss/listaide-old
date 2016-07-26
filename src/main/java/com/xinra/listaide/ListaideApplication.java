0ce54f863f8ea52bb66e7a7fffc2b978e3207afa
Erik Hofer <erik-hofer@mail.de>
2016-07-26 20:35:52 +0200

    Created project using Spring Initializr.

---
+++ src/main/java/com/xinra/listaide/ListaideApplication.java
@@ -0,0 +1,12 @@
+package com.xinra.listaide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaideApplication.class, args);
+	}
+}
---
+++ src/main/java/com/xinra/listaide/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.xinra.listaide;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ListaideApplication.class);
+	}
+
+}
